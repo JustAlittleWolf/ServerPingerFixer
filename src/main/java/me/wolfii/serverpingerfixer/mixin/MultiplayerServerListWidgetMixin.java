6e29c7ebef74695169ba64f030b6e5eef3222a05
Wolfi <wolfi@wolfislaptop>
2026-03-28 10:33:37 +0100

    feat: migrate to mojmap

--- src/main/java/me/wolfii/serverpingerfixer/mixin/MultiplayerServerListWidgetMixin.java
+++ src/main/java/me/wolfii/serverpingerfixer/mixin/MultiplayerServerListWidgetMixin.java
@@ -1,7 +1,6 @@
 package me.wolfii.serverpingerfixer.mixin;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
-import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,40 +9,41 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.List;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor;
+import net.minecraft.client.gui.screens.multiplayer.ServerSelectionList;
 
-@Mixin(MultiplayerServerListWidget.class)
+@Mixin(ServerSelectionList.class)
 public class MultiplayerServerListWidgetMixin {
     @Mutable
     @Final
     @Shadow
-    static ThreadPoolExecutor SERVER_PINGER_THREAD_POOL;
+    static ThreadPoolExecutor THREAD_POOL;
 
     @Unique
     private static final int PINGER_THREAD_COUNT_OVERHEAD = 5;
 
     @Final
     @Shadow
-    private List<MultiplayerServerListWidget.ServerEntry> servers;
+    private List<ServerSelectionList.OnlineServerEntry> onlineServers;
 
     @Unique
     private static boolean threadpoolInitialized = false;
 
-    @Inject(method = "updateEntries", at = @At("HEAD"))
+    @Inject(method = "refreshEntries", at = @At("HEAD"))
     private void updateEntriesInject(CallbackInfo ci) {
         if (!threadpoolInitialized) {
             threadpoolInitialized = true;
             clearServerPingerThreadPool();
         }
-        if (SERVER_PINGER_THREAD_POOL.getActiveCount() >= PINGER_THREAD_COUNT_OVERHEAD) {
+        if (THREAD_POOL.getActiveCount() >= PINGER_THREAD_COUNT_OVERHEAD) {
             clearServerPingerThreadPool();
         }
     }
 
     @Unique
     private void clearServerPingerThreadPool() {
-        SERVER_PINGER_THREAD_POOL.shutdownNow();
-        SERVER_PINGER_THREAD_POOL = new ScheduledThreadPoolExecutor(
-                servers.size() + PINGER_THREAD_COUNT_OVERHEAD,
+        THREAD_POOL.shutdownNow();
+        THREAD_POOL = new ScheduledThreadPoolExecutor(
+                onlineServers.size() + PINGER_THREAD_COUNT_OVERHEAD,
                 (new ThreadFactoryBuilder()).setNameFormat("Server Pinger #%d").setDaemon(true).build()
         );
     }
